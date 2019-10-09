211f4eda936d4658fc3f6550e501fb975113644b
Filip Malinowski <crimhead@gmail.com>
2019-10-09 19:17:46 +0200

    chore: map gh token

--- repo-cooker.js
+++ repo-cooker.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/named
 import { Cooker } from 'repo-cooker';
 
-// process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN
+process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN;
 
 export const cooker = Cooker(process.argv, {
   devtools: {
