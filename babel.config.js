fe095e6bc6f02d72669f337c640e776d7b178051
Filip Malinowski <crimhead@gmail.com>
2020-01-02 02:02:46 +0100

    chore: bump deps; add more babel syntax plugins

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,22 @@
 'use strict'; // eslint-disable-line strict
 
-const MIN_IE_VERSION = 11;
-
 // eslint-disable-next-line import/no-commonjs
 module.exports = api => {
   const isTest = api.cache(() => process.env.NODE_ENV === 'test');
   const babelEnv = api.cache(() => process.env.BABEL_ENV);
   const isNodeTarget = api.cache(() => process.env.BABEL_TARGET === 'node');
 
-  const envTargets =
-    isTest || isNodeTarget ? { node: 'current' } : { ie: MIN_IE_VERSION };
+  const envTargets = isTest || isNodeTarget ? { node: 'current' } : undefined;
 
   const envOpts = {
     loose: true,
     modules: babelEnv === 'cjs' || isTest ? 'commonjs' : false,
-    targets: envTargets
+    targets: envTargets,
+    exclude: [
+      'transform-typeof-symbol',
+      'transform-regenerator',
+      'transform-async-to-generator'
+    ]
   };
 
   const presets = [['@babel/preset-env', envOpts], '@babel/preset-typescript'];
@@ -26,6 +28,30 @@ module.exports = api => {
       {
         regenerator: false,
         useESModules: babelEnv === 'es'
+      }
+    ],
+    !isNodeTarget && [
+      '@babel/plugin-transform-regenerator',
+      {
+        async: false
+      }
+    ],
+    [
+      'transform-async-to-promises',
+      {
+        inlineHelpers: true
+      }
+    ],
+    [
+      '@babel/plugin-proposal-optional-chaining',
+      {
+        loose: false
+      }
+    ],
+    [
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      {
+        loose: false
       }
     ],
     babelEnv === 'cjs' && 'babel-plugin-add-module-exports'
